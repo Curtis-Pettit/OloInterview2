18166cb53ecc2556b3b290e9d0da6b0bd6d6f8cb
Curtis Pettit <Curtis.Pettit@coxautoinc.com>
2020-10-10 09:45:00 -0700

    Add tests for invalid titles and users

--- OloInterviewPart2/PostTests.cs
+++ OloInterviewPart2/PostTests.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using RestSharp;
 using RestSharp.Serializers.NewtonsoftJson;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace OloInterviewPart2
 {
@@ -26,15 +24,121 @@ namespace OloInterviewPart2
             Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
             Assert.IsTrue(response.Data.Id > 0, "Post not created with positive Id");
 
+            var newPost = GetCreatedPost(response.Data.Id);
+            Assert.AreEqual(post.Body, newPost.Body);
+            Assert.AreEqual(post.Title, newPost.Title);
+            Assert.AreEqual(post.UserId, newPost.UserId);
+        }
+
+        [TestMethod]
+        public void IgnorePostIdTest()
+        {
+            var post = new Post
+            {
+                Title = "Test post",
+                Body = "Test body",
+                UserId = 1,
+                Id = 10
+            };
+            RestRequest postRequest = new JsonRequest<Post, Post>(string.Empty, post);
+            RestClient.UseNewtonsoftJson();
+
+            var response = RestClient.Execute<Post>(postRequest, Method.POST);
+            Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
+            Assert.IsTrue(response.Data.Id > 0, "Post not created with positive Id");
+            Assert.AreNotEqual<int>(10, response.Data.Id);
+        }
+
+        [TestMethod]
+        public void EmptyTitleRejectedTest()
+        {
+            var post = new Post
+            {
+                Title = string.Empty,
+                Body = "Test body",
+                UserId = 1
+            };
+
+            RestRequest postRequest = new JsonRequest<Post, Post>(string.Empty, post);
+            RestClient.UseNewtonsoftJson();
+
+            var response = RestClient.Execute<Post>(postRequest, Method.POST);
+            Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.IsNull(response.Data);
+        }
+        //TODO Is there a limit on the number of characters a title or body can contain?
+
+        [TestMethod]
+        public void SpecialCharactersAcceptedTest()
+        {
+            var post = new Post
+            {
+                Title = "ﺧﺨﺩﺪﺫﺬﺭﺮﺯﺰﺱﺲ",
+                Body = "ꏾꏿꐀꐁꐂꐃꐄꐅꐆꐇ ༫༬༭༮༯༰༱༲༳༴༸༺༻༼༽ཀ ᡊᡋᡌᡍᡎᡏᡐᡑᡒᡓᡔᡕᡖᡗᡘᡙᡚᡛ",
+                UserId = 1
+            };
+
+            RestRequest postRequest = new JsonRequest<Post, Post>(string.Empty, post);
+            RestClient.UseNewtonsoftJson();
+
+            var response = RestClient.Execute<Post>(postRequest, Method.POST);
+            var temp = JsonConvert.DeserializeObject<Post>(response.Content);
+            Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
+
+            Assert.IsTrue(response.Data.Id > 0, "Post not created with positive Id");
+
+            var newPost = GetCreatedPost(response.Data.Id);
+            Assert.AreEqual(post.Body, newPost.Body);
+            Assert.AreEqual(post.Title, newPost.Title);
+            Assert.AreEqual(post.UserId, newPost.UserId);
+        }
+
+        [TestMethod]
+        public void MissingUserRejectedTest()
+        {
+            var post = new Post
+            {
+                Title = "Title",
+                Body = "Test body"
+            };
+
+            RestRequest postRequest = new JsonRequest<Post, Post>(string.Empty, post);
+            RestClient.UseNewtonsoftJson();
+
+            var response = RestClient.Execute<Post>(postRequest, Method.POST);
+            Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.IsNull(response.Data);
+        }
+
+        [TestMethod]
+        public void InvalidUserRejectedTest()
+        {
+            var post = new Post
+            {
+                Title = "Title",
+                Body = "Test body",
+                UserId = 1000
+            };
+
+            RestRequest postRequest = new JsonRequest<Post, Post>(string.Empty, post);
+            RestClient.UseNewtonsoftJson();
+
+            var response = RestClient.Execute<Post>(postRequest, Method.POST);
+            Assert.AreEqual(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.IsNull(response.Data);
+        }
+
+
+        private Post GetCreatedPost(int postId)
+        {
             RestRequest getRequest = new RestRequest
             {
-                Resource = response.Data.Id.ToString()
+                Resource = postId.ToString()
             };
+
             var getResponse = RestClient.Execute<Post>(getRequest, Method.GET);
-            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
-            Assert.AreEqual(post.Body, getResponse.Data.Body);
-            Assert.AreEqual(post.Title, getResponse.Data.Title);
-            Assert.AreEqual(post.UserId, getResponse.Data.UserId);
-        }    
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, getResponse.StatusCode, $"Error retreiveing the post just created with Id {postId}");
+            return getResponse.Data;
+        }
     }
 }
